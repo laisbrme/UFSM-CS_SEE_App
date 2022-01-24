83d5e99611bb42e0b3b48c809d98ff1a6303fc60
laisbrme <52614305+laisbrme@users.noreply.github.com>
2022-01-24 15:34:30 -0300

    teste container

--- src/menuModelos/barramentos/1/1.py
+++ src/menuModelos/barramentos/1/1.py
@@ -10,13 +10,16 @@ app = Tk()
 app.title("Barramento Simples - Manobra no Disjuntor")
 #app.minsize(width=150, height=150)
 
+fr_quadro = Frame(app, borderwidth = 1, relief = "solid")
 
 container1 = Config()
 container2 = Elementos()
 
-container1.imagem(app, texto='BARRA SIMPLES', caminho='1.BarraSimples.gif')
-#container2.criaBotoesCheck(app, numS=10, numD=5, numTC=5, numTP=2, numRele=1)
+container1.imagem(, texto='BARRA SIMPLES', caminho='1.BarraSimples.gif')
+
+container2.criaBotoesCheck(, numS=10, numD=5, numTC=5, numTP=2, numRele=1)
 
 Button(app,text='Energizar',command=app.destroy).grid(row=12, column=0, sticky=E)
 Button(app,text='Fechar',command=app.destroy).grid(row=12, column=1, sticky=E)
+
 app.mainloop()
