28e06b67ac177acf0ee13cf2fe78892fbc02e786
laisbrme <52614305+laisbrme@users.noreply.github.com>
2022-04-25 16:44:26 -0300

    comit

--- UFSM_SEE_app.py
+++ UFSM_SEE_app.py
@@ -9,7 +9,7 @@ from src.__init__ import *
 app = Tk()
 app.title("UFSM-CS SEE")
 app.iconbitmap("ufsm-see.ico")
-#app.wm_state('zoomed')
+app.wm_state('zoomed')
 app.configure(background="#dde")
 app.minsize(width=300, height=200)
 
@@ -19,3 +19,7 @@ menus.criaBarraMenus(app)
 status = 'Inicie uma nova manobra'
 Label(app, text=status, bd=1, relief=SUNKEN, anchor=W).pack(side=BOTTOM, fill=X)
 app.mainloop()
+
+
+
+
