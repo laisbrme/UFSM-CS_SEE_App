873d2c048596a5fb0039253daf4a4b7f64df6d38
laisbrme <52614305+laisbrme@users.noreply.github.com>
2022-05-22 23:50:48 -0300

    Bar1 e Bar6
    Muitos progressos com a barra 6, barra 1 serviu para teste

--- src/menuModelos/barras/barra1.py
+++ src/menuModelos/barras/barra1.py
@@ -1,26 +1,111 @@
-'''
-from src.menuModelos.manobrasBarras import *
+from tkinter import *
 
-titulo = "Barramento Simples"
+app = Tk()
+app.title("Barramento Simples")
+app.iconbitmap('img/ufsm-see.ico')
+
+def cliclouStart():
+    container1 = Frame(app, bg="black", bd=4, relief=GROOVE)
+    container2 = Frame(app, bg="black", bd=4, relief=GROOVE)
+    subcontainer1 = Frame(container2, bg="#dde", bd=4, relief=RAISED)
+    subcontainer2 = Frame(container2, bd=4)
+    subcontainer3 = Frame(container2, bd=4)
+    container1.pack(side='left')
+    container2.pack(side='left', fill='y')
+    subcontainer1.pack(fill='both', expand='yes')
+    subcontainer2.pack(fill='both', expand='yes')
+    subcontainer3.pack(fill='both', expand='yes')
+
+    # Objeto imagem:
+    dir = 'img/bar6.png'
+    img = PhotoImage(file=dir)
+    diagrama = Label(container1, image=img)
+    diagrama.pack(fill='both')
+
+    # Criação Tela Interativa
+    alerta = '--------------------------------------------------------------\n' \
+             'Barramento Duplo com Disjuntor Duplo\n\n' \
+             'Defina o elemento com falha!\n' \
+             '--------------------------------------------------------------'
+    telaInt = Label(subcontainer1,
+                    text=alerta,
+                    font='times 12 bold',
+                    justify='center',
+                    bg="#dde")
+    telaInt.pack(fill='both', expand='yes')
+
+    # Criação Combobox
+    opcoes = ['Disjuntor D1',
+              'Disjuntor D2',
+              'Disjuntor D3',
+              'Disjuntor D4',
+              'Disjuntor D5',
+              'Disjuntor D6',
+              'Disjuntor D7',
+              'Disjuntor D8', ]
+
+    clicada = StringVar()
+    Label(subcontainer2, text="Elemento: ").pack(fill='both', side='left', padx=5, pady=5)
+    combo = OptionMenu(subcontainer2, clicada, *opcoes)
+    combo.pack(fill='x', side='left', padx=5, pady=5)
+
+    # Criação Botões
+    botao1 = Button(subcontainer3, text='Próximo Passo', command='1')
+    botao1.pack(side='left', fill='x', expand='yes')
+    botao2 = Button(subcontainer3, text='Finalizar Manobra')
+    botao2.pack(side='left', fill='x', expand='yes')
+
+    if combo.get() == 'Disjuntor D3' and botao1.get() == '1':
+        # Objeto imagem:
+        dir = 'img/bar5.png'
+        img = PhotoImage(file=dir)
+        diagrama = Label(container1, image=img)
+        diagrama.pack(fill='both')
+
+        # Criação Tela Interativa
+        alerta = '--------------------------------------------------------------\n' \
+                 'Barramento Duplo com Disjuntor Duplo\n\n' \
+                 'Falha em D3!\n' \
+                 '--------------------------------------------------------------'
+        telaInt = Label(subcontainer1,
+                        text=alerta,
+                        font='times 12 bold',
+                        justify='center',
+                        bg="#dde")
+        telaInt.pack(fill='both', expand='yes')
+
+        # Criação Combobox
+        opcoes = ['Abrir S1-2',
+                  'Abrir S5-6',
+                  'Abrir S9-10',
+                  'Abrir S13-14',
+                  'Fechar S3-4',
+                  'Fechar S7-8',
+                  'Fechar S11-12',
+                  'Fechar S15-16',
+                  'Desligar D1',
+                  'Desligar D3',
+                  'Desligar D5',
+                  'Desligar D7',
+                  'Ligar D2',
+                  'Ligar D4',
+                  'Ligar D6',
+                  'Ligar D8',]
+
+        clicada = StringVar()
+        Label(subcontainer2, text="Elemento: ").pack(fill='both', side='left', padx=5, pady=5)
+        combo = OptionMenu(subcontainer2, clicada, *opcoes)
+        combo.pack(fill='x', side='left', padx=5, pady=5)
+
+        # Criação Botões
+        botao1 = Button(subcontainer3, text='Próximo Passo')
+        botao1.pack(side='left', fill='x', expand='yes')
+        botao2 = Button(subcontainer3, text='Finalizar Manobra')
+        botao2.pack(side='left', fill='x', expand='yes')
 
 
-app.title(titulo)
-diretorio = 'C:/Users/laisb/Documents/GitHub/UFSM-CS_SEE_App/ufsm-see.ico'
-app.iconbitmap(diretorio)
 
-Label(app, text=titulo, font='Times 14 bold').pack()
 
-img = PhotoImage(file='img/bar1.png')  # objeto imagem
-Label(app, image=img).pack(expand=YES, fill=BOTH)
+botaoStart = Button(app, text='Iniciar Manobra', command=cliclouStart).pack(side='left', fill='x', expand='yes')
 
-Label(app, text='Elementos:', font='Times 12 bold').pack(anchor=NW)
-container2 = Elementos()
-container2.criaBotoesCheck(app, numS=10, numD=5, numTC=4, numTP=1, numRele=1)
-
-container3 = Frame(app)
-container3.pack(side=BOTTOM)
-Button(container3, text='Energizar', command=app.destroy).pack(side=LEFT)
-Button(container3, text='Fechar', command=app.destroy).pack(side=RIGHT)
-
-'''
-
+app.mainloop()
--- src/menuModelos/barras/barra6.py
+++ src/menuModelos/barras/barra6.py
@@ -1,19 +1,121 @@
 import os
-from tkinter import *
 from tkinter import ttk
-from src.menuModelos.manobrasBarras import *
-from PIL import ImageTk, Image
-
-class JanBar6:
-    def configBar6(self, toplevel, status):
-        self.cavidade = Frame(toplevel)
-        self.cavidade.pack()
+from tkinter import *
+from tkinter import messagebox
 
 
-        image = PhotoImage(file='src/menuModelos/barras/img/bar6.png')  # objeto imagem
-        #Button(toplevel, text = '  ', image = image).pack(side = TOP)
-        Label(toplevel, image=image).pack(expand=YES, fill=BOTH)
+app = Tk()
+app.title("Barramento Duplo com Disjuntor Duplo")
+app.iconbitmap('img/ufsm-see.ico')
 
-        self.operacoes = Button(self.cavidade, text='B3').pack(side=LEFT)
 
-        Label(toplevel, text=status, bd=1, relief=SUNKEN, anchor=W).pack(side=BOTTOM, fill=X)
+def analiseD1(diagrama, telaInt, combo, botao1):
+    pass
+
+def analiseD3(diagrama, telaInt, combo, botao1):
+    #newImage = PhotoImage(file='img/bar5.png')
+    #diagrama.configure(image=newImage)
+    telaInt.configure(text='Falha no Disjuntor D3\nSelecione o próximo passo:')
+    combo.configure(values=['Abrir S1-2',
+                            'Abrir S5-6',
+                            'Abrir S9-10',
+                            'Abrir S13-14',
+                            'Fechar S3-4',
+                            'Fechar S7-8',
+                            'Fechar S11-12',
+                            'Fechar S15-16',
+                            'Desligar D1',
+                            'Desligar D3',
+                            'Desligar D5',
+                            'Desligar D7',
+                            'Ligar D2',
+                            'Ligar D4',
+                            'Ligar D6',
+                            'Ligar D8', ])
+    botao1.configure(text='Próximo Passo', command= lambda:analiseD3(diagrama, telaInt, combo, botao1))
+    botao2.configure(text='Finalizar Manobra')
+    while combo.get() != 'Fechar S3-4' and botao1.bind() == True:
+        print('entrou no while')
+        telaInt.configure(text='Seleção inválida!\nSelecione o próximo passo:')
+    else:
+        print('não entrou no while')
+
+
+def analiseD5(diagrama, telaInt, combo, botao1):
+    pass
+
+def analiseD7(diagrama, telaInt, combo, botao1):
+    pass
+
+def cliclouB1(diagrama, telaInt, combo, botao1):
+    print('Entrou na função clicou botão 1')
+    ElemSel = combo.get()
+    if ElemSel == 'Disjuntor D1':
+        print(ElemSel)
+        analiseD1(diagrama, telaInt, combo, botao1)
+
+    elif ElemSel == 'Disjuntor D3':
+        print(ElemSel)
+        analiseD3(diagrama, telaInt, combo, botao1)
+
+    elif ElemSel == 'Disjuntor D5':
+        print(ElemSel)
+        analiseD5(diagrama, telaInt, combo, botao1)
+
+    elif ElemSel == 'Disjuntor D7':
+        print(ElemSel)
+        analiseD7(diagrama, telaInt, combo, botao1)
+
+
+container1 = Frame(app, bd=4, relief=GROOVE)
+container2 = Frame(app, bd=4, relief=GROOVE)
+subcontainer1 = LabelFrame(container2, bg="#dde", bd=4, relief=GROOVE)
+subcontainer2 = LabelFrame(container2, text='Elemento(s)', bd=4)
+subcontainer3 = LabelFrame(container2, bd=4)
+container1.pack(side='left', padx=10, pady=10)
+container2.pack(side='left', fill='y', padx=10, pady=10)
+subcontainer1.pack(fill='both', expand='yes', padx=10, pady=10)
+subcontainer2.pack(fill='both', expand='yes', padx=10, pady=10)
+subcontainer3.pack(fill='both', expand='yes', padx=10, pady=10)
+
+
+# Objeto imagem:
+img = PhotoImage(file='img/bar6.png')
+diagrama = Label(container1, image=img)
+
+# Criação Tela Interativa
+telaInt = Label(subcontainer1,
+                     text='--------------------------------------------------------------\n' \
+                          'Barramento Duplo com Disjuntor Duplo\n\n' \
+                          'Defina o elemento com falha!\n' \
+                          '--------------------------------------------------------------',
+                     font='times 12 bold',
+                     justify='center',
+                     bg="#dde")
+
+# Criação Combobox
+clicada = StringVar()
+combo = ttk.Combobox(subcontainer2, textvariable=clicada, state='readonly')
+combo['values'] = ['Disjuntor D1',
+                   'Disjuntor D3',
+                   'Disjuntor D5',
+                   'Disjuntor D7',]
+
+# Criação Botões
+botao1 = Button(subcontainer3, text='Iniciar Manobra', command= lambda:cliclouB1(diagrama, telaInt, combo, botao1))
+botao2 = Button(subcontainer3, text='Sair', command=lambda:app.destroy())
+
+# Exibição
+diagrama.pack(fill='both')
+telaInt.pack(fill='both', expand='yes', padx=10, pady=10)
+combo.pack(fill='x', side='left', padx=10, pady=10)
+botao1.pack(side='left', fill='x', expand='yes', padx=10, pady=10)
+botao2.pack(side='left', fill='x', expand='yes', padx=10, pady=10)
+
+
+#combo.bind('<<ComboboxSelected>>', analise)
+#botao1.bind('<Button-1>', True)
+#botao2.bind('<Button-1>', app.destroy())
+
+
+app.mainloop()
